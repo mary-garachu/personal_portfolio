fa0cf6f8d6aa480a08f202d16de18fb885200cbe
Sonie <garachu.muthoni@gmail.com>
2024-10-15 14:41:35 +0300

    update header title

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className='Header'>
       <div className='Header-content'>
-        <h1> Hi I'M <span> MARY MUTHONI </span> </h1>
+        <h1> HI I'M <span> MARY MUTHONI </span> </h1>
         <h2> I am a Web Developer </h2>
         <p> I specialize in building user-friendly and responsive web applications, combining creativity with technical expertise to deliver high-quality solutions.</p>
       </div>
